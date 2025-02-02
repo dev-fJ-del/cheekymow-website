e7f8d9c470aad5cf09eadccfeaec6dd61e2c642c
Frans <frans.delport@mobility.com.au>
2025-02-02 16:32:32 +0800

    Improved loading speed
    - Improved contrast
    - Added resource caching.

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "cheekymow-cache-v1";
+const urlsToCache = [
+  "/",
+  "/styles.css",
+  "/fonts/DIN%20Medium.ttf",
+  "/fonts/Arima-SemiBold.ttf",
+  "/fonts/Font%20Awesome%206%20Brands.otf",
+  "/images/cheekymow_logo.webp",
+  "/images/features/image1.webp",
+  "/images/features/image2.webp",
+  "/images/features/image3.webp",
+  "/images/features/image4.webp",
+  "/images/features/image5.webp",
+  "/images/features/feature-graphic.webp",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
